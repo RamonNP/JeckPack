bae1ca15533606100cdc4f5f3a3c039c17787f12
Ramon Pereira <ramon.pereira3156@gmail.com>
2020-06-22 21:00:34 -0300

    JackPack-configuração inicial

--- JeckPack/JeckPack/Assets/Scripts/JeckPackController.cs
+++ JeckPack/JeckPack/Assets/Scripts/JeckPackController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class JeckPackController : MonoBehaviour
 {
+    private uint coins = 0;
+    private bool isDead = false;
     public float jetpackForce = 75.0f;
     private Rigidbody2D playerRigidbody;
-    public float velocidadeMovimento = 3f;
+    public float forwardMovementSpeed = 3f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,13 +18,45 @@ public class JeckPackController : MonoBehaviour
     void FixedUpdate()
     {
         bool jetpackActive = Input.GetButton("Fire1");
+        jetpackActive = jetpackActive && !isDead;
+
         if (jetpackActive)
         {
             playerRigidbody.AddForce(new Vector2(0, jetpackForce));
         }
-        Vector2 newVelocity = playerRigidbody.velocity;
-        newVelocity.x = velocidadeMovimento;
-        playerRigidbody.velocity = newVelocity;
+
+        if (!isDead)
+        {
+            Vector2 newVelocity = playerRigidbody.velocity;
+            newVelocity.x = forwardMovementSpeed;
+            playerRigidbody.velocity = newVelocity;
+        }
+
+        //UpdateGroundedStatus();
+        //AdjustJetpack(jetpackActive);
 
     }
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Moeda"))
+        {
+            CollectCoin(collider);
+        }
+        else if(collider.gameObject.CompareTag("Dano"))
+        {
+            HitByLaser(collider);
+        }
+    }
+
+    void HitByLaser(Collider2D laserCollider)
+    {
+        isDead = true;
+        //mouseAnimator.SetBool("isDead", true);
+    }
+    void CollectCoin(Collider2D coinCollider)
+    {
+        coins++;
+        Destroy(coinCollider.gameObject);
+    }
+
 }
